04bb916dec65d5ea86166739f2321bf46fc995ca
Parikshit Shashtri <parikshitshashtri@Parikshits-MacBook-Air.local>
2024-08-20 21:18:17 +0530

    Database Postgres with Prisma setup

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import express , {Express,Request,Response} from 'express';
-
+import 'dotenv/config';
 const app:Express = express() 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.get('/',(req:Request,res:Response) => { 
     res.send('Dashboard Backend')
 }) 
+app.get('/home',(req:Request,res:Response) => { 
+    res.send('Homeboard')
+})
 app.listen(PORT,() => { 
     console.log(`Server is started & running on this ${PORT}`);
     
