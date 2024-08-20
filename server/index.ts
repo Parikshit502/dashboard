5b95e83a1d7aa5c9b81de6f544825fabb6625d63
Parikshit Shashtri <parikshitshashtri@Parikshits-MacBook-Air.local>
2024-08-20 19:37:57 +0530

    Typescript Packages are updated

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,11 @@
+import express , {Express,Request,Response} from 'express';
 
-
+const app:Express = express() 
+const PORT = 3000;
+app.get('/',(req:Request,res:Response) => { 
+    res.send('Dashboard Backend')
+}) 
+app.listen(PORT,() => { 
+    console.log(`Server is started & running on this ${PORT}`);
+    
+}) 
